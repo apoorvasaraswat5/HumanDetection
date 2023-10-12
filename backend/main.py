5612ba1815d369a866deb803f4aa6b99ab32f2f1
Apoorva Saraswat <apoorvasaras@umass.edu>
2023-10-11 20:14:05 -0400

    added code for video model

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,9 @@
-from typing import Union
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
 from dotenv import load_dotenv  # Corrected import statement
 import requests
 import os
+import io
+from google.cloud import videointelligence_v1 as videointelligence
 
 load_dotenv()
 
@@ -27,3 +27,94 @@ def query(filename: str):
             status_code=500,
             detail=f"Failed to connect to huggingface inference API: {e}",
         )
+
+
+@app.post("/video")
+def detect_person(local_file_path: str):
+    """Detects people in a video from a local file."""
+
+    response = dict()
+
+    client = videointelligence.VideoIntelligenceServiceClient()
+
+    with io.open(local_file_path, "rb") as f:
+        input_content = f.read()
+
+    # Configure the request
+    config = videointelligence.types.PersonDetectionConfig(
+        include_bounding_boxes=True,
+        include_attributes=True,
+        include_pose_landmarks=True,
+    )
+    context = videointelligence.types.VideoContext(person_detection_config=config)
+
+    # Start the asynchronous request
+    operation = client.annotate_video(
+        request={
+            "features": [videointelligence.Feature.PERSON_DETECTION],
+            "input_content": input_content,
+            "video_context": context,
+        }
+    )
+
+    print("\nProcessing video for person detection annotations.")
+    result = operation.result(timeout=300)
+
+    print("\nFinished processing.\n")
+
+    # Retrieve the first result, because a single video was processed.
+    annotation_result = result.annotation_results[0]
+
+    response["person_detected"] = list()
+    for annotation in annotation_result.person_detection_annotations:
+        print("Person detected:")
+        for track in annotation.tracks:
+            print(
+                "Segment: {}s to {}s".format(
+                    track.segment.start_time_offset.seconds
+                    + track.segment.start_time_offset.microseconds / 1e6,
+                    track.segment.end_time_offset.seconds
+                    + track.segment.end_time_offset.microseconds / 1e6,
+                )
+            )
+            response["person_detected"].append(
+                "Segment: {}s to {}s".format(
+                    track.segment.start_time_offset.seconds
+                    + track.segment.start_time_offset.microseconds / 1e6,
+                    track.segment.end_time_offset.seconds
+                    + track.segment.end_time_offset.microseconds / 1e6)
+            )
+            # Each segment includes timestamped objects that include
+            # characteristic - -e.g.clothes, posture of the person detected.
+            # Grab the first timestamped object
+            timestamped_object = track.timestamped_objects[0]
+            box = timestamped_object.normalized_bounding_box
+            print("Bounding box:")
+            print("\tleft  : {}".format(box.left))
+            print("\ttop   : {}".format(box.top))
+            print("\tright : {}".format(box.right))
+            print("\tbottom: {}".format(box.bottom))
+
+            # Attributes include unique pieces of clothing,
+            # poses, or hair color.
+            print("Attributes:")
+            for attribute in timestamped_object.attributes:
+                print(
+                    "\t{}:{} {}".format(
+                        attribute.name, attribute.value, attribute.confidence
+                    )
+                )
+
+            # Landmarks in person detection include body parts such as
+            # left_shoulder, right_ear, and right_ankle
+            print("Landmarks:")
+            for landmark in timestamped_object.landmarks:
+                print(
+                    "\t{}: {} (x={}, y={})".format(
+                        landmark.name,
+                        landmark.confidence,
+                        landmark.point.x,  # Normalized vertex
+                        landmark.point.y,  # Normalized vertex
+                    )
+                )
+    return response
