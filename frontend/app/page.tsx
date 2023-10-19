247429ca2ef10a15bb97c12a815c881f48aa5e1c
nekomi2 <dore123222@gmail.com>
2023-10-19 13:21:39 -0400

    mockUI

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,93 +1,93 @@
+"use client"
 import Card from "@/components/Card";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/Dialog";
+import { useState } from "react";
 export default function Home() {
+  const [Videos, setVideos] = useState([
+    "Video1.mp4",
+    "Video2.mp4",
+    "Video3.mp4",
+    "Video4.mp4",
+    "Video5.mp4",
+    "Video6.mp4",
+    "Video7.mp4",
+  ]);
+  const images = [
+    "https://i.imgur.com/XgbZdeA.jpeg",
+    "https://i.imgur.com/3FVUanX.jpeg",
+  ];
+  const [imgIndex, setImgIndex] = useState(0);
+  const [open, setOpen] = useState(false);
+  const onClick = () => {
+    setImgIndex((imgIndex + 1) % images.length);
+  };
+  const openAudio = () => {
+    console.log(`open:`);
+    setOpen(!open);
+  }
   return (
     <div className="main-content flex h-screen">
       <div className=" sidebar w-1/4 space-y-3 bg-gray-300 overflow-scroll">
-        <Card fileName="Video1.mp4" date="01/01/2024" size="100MB"/>
-        <Card fileName="Video2.mp4" date="02/01/2024" size="100MB"/>
-        <Card fileName="Video3.mp4" date="03/01/2024" size="100MB"/>
-        <Card fileName="Video4.mp4" date="04/01/2024" size="100MB"/>
-        <Card fileName="Video5.mp4" date="04/01/2024" size="100MB"/>
-        <Card fileName="Video6.mp4" date="04/01/2024" size="100MB"/>
-        <Card fileName="Video7.mp4" date="04/01/2024" size="100MB"/>
+        {Videos.map((video) => {
+          return <Card onClick={onClick} fileName={video} key={video} size="100MB"/>;
+        })}
       </div>
       <div className="content w-3/4 flex flex-col p-2">
         <h2 className="text-2xl font-bold tracking-tight text-gray-900">
           Images
         </h2>
         <div className="flex flex-row p-5 space-x-5">
-          <img
-            className="h-[200px] w-[250px] max-w-lg rounded-lg"
-            alt=""
-            src="https://i.imgur.com/XgbZdeA.jpeg"
-          />
-          <img
-            className="h-[200px] w-[250px] max-w-lg rounded-lg"
-            alt=""
-            src="https://i.imgur.com/XgbZdeA.jpeg"
-          />
-          <img
-            className="h-[200px] w-[250px] max-w-lg rounded-lg"
-            alt=""
-            src="https://i.imgur.com/XgbZdeA.jpeg"
-          />
-          <img
-            className="h-[200px] w-[250px] max-w-lg rounded-lg"
-            alt=""
-            src="https://i.imgur.com/XgbZdeA.jpeg"
-          />
+          {Array(4)
+            .fill(0)
+            .map((_, index) => {
+              return (
+                <img
+                  className="h-[200px] w-[250px] max-w-lg rounded-lg"
+                  alt=""
+                  src={images[imgIndex]}
+                  key={index}
+                />
+              );
+            })}
         </div>
         <h2 className="text-2xl font-bold tracking-tight text-gray-900">
           Audio
         </h2>
         <div className="flex flex-row p-5 space-x-5">
-          <img
-            className="h-[200px] w-[250px] max-w-lg rounded-lg"
-            alt=""
-            src="https://i.imgur.com/XgbZdeA.jpeg"
-          />
-          <img
-            className="h-[200px] w-[250px] max-w-lg rounded-lg"
-            alt=""
-            src="https://i.imgur.com/XgbZdeA.jpeg"
-          />
-          <img
-            className="h-[200px] w-[250px] max-w-lg rounded-lg"
-            alt=""
-            src="https://i.imgur.com/XgbZdeA.jpeg"
-          />
-          <img
-            className="h-[200px] w-[250px] max-w-lg rounded-lg"
-            alt=""
-            src="https://i.imgur.com/XgbZdeA.jpeg"
-          />
+          {Array(4)
+            .fill(0)
+            .map((_, index) => {
+              return (
+                <img
+                  onClick={openAudio}
+                  className="h-[200px] w-[250px] max-w-lg rounded-lg"
+                  alt=""
+                  src={images[imgIndex]}
+                  key={index}
+                />
+              );
+            })}
         </div>
         <h2 className="text-2xl font-bold tracking-tight text-gray-900">
           People
         </h2>
         <div className="flex flex-row p-5 space-x-5">
-          <img
+          {Array(4).fill(0).map((_, index) => {
+            return (
+              <img
             className="h-[200px] w-[200px] max-w-lg rounded-full"
             alt=""
-            src="https://i.imgur.com/XgbZdeA.jpeg"
+            src={images[imgIndex]}
+            key={index}
           />
-          <img
-            className="h-[200px] w-[200px] max-w-lg rounded-full"
-            alt=""
-            src="https://i.imgur.com/XgbZdeA.jpeg"
-          />
-          <img
-            className="h-[200px] w-[200px] max-w-lg rounded-full"
-            alt=""
-            src="https://i.imgur.com/XgbZdeA.jpeg"
-          />
-          <img
-            className="h-[200px] w-[200px] max-w-lg rounded-full"
-            alt=""
-            src="https://i.imgur.com/XgbZdeA.jpeg"
-          />
+            )
+          })}
         </div>
       </div>
+      {/* <Dialog>
+    <DialogTrigger>Dialog trigger</DialogTrigger>
+    <DialogContent>Dialog Content</DialogContent>
+  </Dialog> */}
     </div>
   );
 }
--- frontend/components/Card.tsx
+++ frontend/components/Card.tsx
@@ -1,18 +1,29 @@
-export default function Card({thumbnail="/defaultThumbnail.png", fileName='video.mp4', date='01/01/2020', size='0kb'}) {
+export default function Card({
+  onClick,
+  thumbnail = "/defaultThumbnail.png",
+  fileName = "video.mp4",
+  date = "01/01/2020",
+  size = "0kb",
+}: {
+  onClick: () => void;
+  thumbnail?: string;
+  fileName?: string;
+  date?: string;
+  size?: string;
+}) {
   return (
-    <a
-      href="#"
-      className="group items-center p-6 bg-white border border-gray-200 shadow hover:bg-blue-200 flex space-x-5"
+    <button
+      onClick={onClick}
+      className="w-full group items-center p-6 bg-white border border-gray-200 shadow hover:bg-blue-200 flex space-x-5"
     >
-    <img src={thumbnail}/>
+      <img src={thumbnail} />
       <div className="mb-2 text-lg text-gray-900">
         <div className="font-bold">{fileName}</div>
         <div>{date}</div>
       </div>
-      <div className="filesize border-2 p-2 mr-auto group-hover:border-black">
+      <div className="filesize border-2 p-2 ml-auto group-hover:border-black">
         {size}
-      </div> 
-      
-    </a>
+      </div>
+    </button>
   );
 }
