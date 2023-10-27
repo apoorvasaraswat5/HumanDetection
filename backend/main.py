d950c5ecf4e007062f8d1ac020d7f90f14721ac6
Xingyu Bian <xingyubian@umass.edu>
2023-10-27 15:44:07 -0400

    implemented custom diarization whisper pipeline

--- backend/main.py
+++ backend/main.py
@@ -7,16 +7,13 @@ from dotenv import load_dotenv  # Corrected import statement
 import requests
 import utils
 import os
-import torch
 import cv2
 import imutils
 from pyannote.audio import Pipeline
 from transformers import pipeline
-from speechbox import ASRDiarizationPipeline
-from datasets import load_dataset
-from utils import convert_to_wav
-from transformers import pipeline
 from gotrue.errors import AuthApiError
+from utils import convert_to_wav
+from whisper_diarization import whisper_diarization
 
 # Start backend inside file
 import uvicorn
@@ -155,33 +152,7 @@ def diarize(filename: str):
 
 @app.post("/transcribe")
 def transcribe(filename: str):
-    # get pretrained diarization pipeline
-    diarization_pipeline = Pipeline.from_pretrained(
-        "pyannote/speaker-diarization-3.0",
-        use_auth_token=os.getenv("HF_KEY"),
-    )
-
-    # get pretrained asr pipeline
-    asr_pipeline = pipeline(
-        "automatic-speech-recognition",
-        model="openai/whisper-base",
-    )
-
-    # load dataset of concatenated LibriSpeech samples
-    concatenated_librispeech = load_dataset(
-        "sanchit-gandhi/concatenated_librispeech", split="train", streaming=True
-    )
-    # get first sample
-    sample = next(iter(concatenated_librispeech))
-
-    # get composite pipeline
-    comp_pipeline = ASRDiarizationPipeline(
-        asr_pipeline=asr_pipeline, diarization_pipeline=diarization_pipeline
-    )
-
-    output = comp_pipeline(sample["audio"])
-
-    return output
+    return whisper_diarization(filename)
 
 
 @app.post("/video")
