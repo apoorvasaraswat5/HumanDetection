bc985febc37fe9e9ef6e2b154fa1bde942f3c2a1
rwbakerUMASS <rwbaker@umass.edu>
2023-10-26 20:28:32 -0400

    Get file list, remove dummy data

--- frontend/app/upload/page.tsx
+++ frontend/app/upload/page.tsx
@@ -1,44 +1,39 @@
 "use client"
 import Card from "@/components/Card";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/Dialog";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect, useCallback } from "react";
 import style from 'app/upload/upload.module.css';
 import RecentUpload from "./components/RecentUpload";
+import { json } from "stream/consumers";
 
 export default function page() {
 
+  const [recentVideos, setRecentVideos] = useState([{"name":"","date":"","size":0}]);
+
+
   const getRecent = () => {
-    // TODO
-    // Temp values currently 
-    const tempValues = [
-        {"name":"MaurisLaoreet.mp3","date":"11/03/2022","size":11.4},
-        {"name":"MontesNasceturRidiculus.mp3","date":"07/30/2023","size":21.7},
-        {"name":"Vel.ppt","date":"09/27/2023","size":32.2},
-        {"name":"PenatibusEt.mp3","date":"07/29/2023","size":8.0},
-        {"name":"NecSed.jpeg","date":"11/14/2022","size":20.7},
-        {"name":"VenenatisNon.xls","date":"03/27/2023","size":0.9},
-        {"name":"SuscipitA.avi","date":"09/26/2023","size":9.7},
-        {"name":"QuamPedeLobortis.ppt","date":"02/05/2023","size":13.1},
-        {"name":"VulputateVitae.xls","date":"04/05/2023","size":5.7},
-        {"name":"ErosViverra.tiff","date":"01/25/2023","size":15.6},
-        {"name":"VitaeNislAenean.xls","date":"12/26/2022","size":22.6},
-        {"name":"EuOrci.gif","date":"12/14/2022","size":13.7},
-        {"name":"AcNulla.jpeg","date":"04/02/2023","size":3.4},
-        {"name":"ArcuSedAugue.mov","date":"07/08/2023","size":12.3},
-        {"name":"LacusMorbi.mp3","date":"04/29/2023","size":31.6},
-        {"name":"EuTincidunt.ppt","date":"10/06/2023","size":16.9},
-        {"name":"Cras.avi","date":"09/05/2023","size":16.9},
-        {"name":"Id.png","date":"09/01/2023","size":30.0},
-        {"name":"Pellentesque.mp3","date":"10/28/2022","size":33.1},
-        {"name":"Sapien.mov","date":"09/01/2023","size":12.1},
-        {"name":"Faucibus.png","date":"11/09/2022","size":30.5},
-        {"name":"Praesent.xls","date":"11/27/2022","size":33.3},
-        {"name":"OdioDonecVitae.ppt","date":"10/01/2023","size":14.5},
-        {"name":"NecCondimentum.avi","date":"01/15/2023","size":7.9},
-        {"name":"JustoIn.avi","date":"01/24/2023","size":3.4}
-    ];
-    return tempValues;
-  }
+    let vals: any[] = []
+    let res = fetch('http://127.0.0.1:8000/fetchData', {
+        method: 'GET'
+    }).then((res) => {
+      res.json().then((body) =>{
+        body.data.forEach((video: { filename: string; created_at: string }) => 
+        {
+          vals.push({
+            "name":video.filename,
+            "date":video.created_at,
+            "size":0
+          });
+        });
+      });
+    });
+    return vals;
+  };
+
+  useEffect(() => {
+    const vals = getRecent();
+    setRecentVideos(() => vals);
+  },[])
 
   const sendFiles = (files: any) => {
     console.log(files)
@@ -119,7 +114,6 @@ export default function page() {
   const handleInput = (event: any) => {
     sendFiles(event.target.files);
   }
-
   return (
     <div className="main-content flex h-screen">
         <div className={style.topnav}>
@@ -131,7 +125,7 @@ export default function page() {
         {recentIsActive ?(
           <div id="recentlist" style={{marginTop: 40}} className="w-full">
               {
-                getRecent().sort((a,b) =>{
+                recentVideos.sort((a,b) =>{
                   return new Date(b.date).getTime() - new Date(a.date).getTime();
                 }).map((video) => {
                   return <RecentUpload onClick={handleClick} fileName={video.name} size={video.size + ' GB'} date={video.date} key={video.name}/>;
