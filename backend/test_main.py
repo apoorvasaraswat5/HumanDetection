3c2b8f775c8bfffdc2c93eeb2eb1eac13912ecc2
Krisna Lach <lach.krisna@gmail.com>
2023-11-02 12:57:09 -0400

    Update test_main.py
    Completing some remote work

--- backend/test_main.py
+++ backend/test_main.py
@@ -1,9 +1,9 @@
-import sklearn  # put in requirements.txt
+#import sklearn  # put in requirements.txt
 import sys
-import simpleder  # put in reqruirements.txt
+#import simpleder  # put in reqruirements.txt
 import os
 
-from whisper_diarization import whisper_diarization
+#from whisper_diarization import whisper_diarization
 
 
 def test_audio():
@@ -15,9 +15,10 @@ def test_audio():
     # define folder paths
     reference_folder = "HumanDetection/dataset/references"
     audio_folder = "HumanDetection/dataset/audio_files"
-    # get reference and audio files
-    references = [file for file in os.listdir(reference_folder)]
-    audio_files = [file for file in os.listdir(audio_folder)]
+
+    # get reference and audio files (unsure if this always works)
+    references = sorted([file for file in os.listdir(reference_folder)])
+    audio_files = sorted([file for file in os.listdir(audio_folder)])
 
     for reference, audio_file in zip(references, audio_files):
         # construct file paths
@@ -25,19 +26,41 @@ def test_audio():
         audio_path = os.path.join(audio_folder, audio_file)
 
         # feed audio into pipeline
-        res = whisper_diarization(audio_path)
+        #res = whisper_diarization(audio_path)
 
-        with open(reference_path, "r") as ref:
-            print(ref.readline())
+        # get cleaned up reference
+        clean_rttm(reference_path)
+
+        
 
 
-def clean_rttm():
+def clean_rttm(reference_path):
     """
     Clean up an .rttm file for evaluation tasks
-    Args:
-    Returns:
+    Args: file - file path for a .rttm file
+    Returns: a list of tuples of form:
+            (speaker id, time start, time end)
     """
+    res = []
+    running_time = 0.00 # keep track of time from 0.00
+    with open(reference_path, "r") as ref:
+        for line in ref:
+            temp = []
+            clean_line = line.split()
+            # get rid of irrelevant fields
+            # see https://stackoverflow.com/a/74358577 for more about rttm
+            for i, elem in enumerate(clean_line):
+                # get timestamps
+                if i in [3, 4]:
+                    temp.append(float(elem))
+                # get speaker id in correct format
+                if i == 7:
+                    temp.append(elem[3:] if elem[3] != "0" else elem[4:])
+            temp = temp[::-1]
 
+            print(temp)
+
+            
 
 def main():
     test_audio()
