e54d71559c79e7e9c159668ed7a0b4aab2f5a61e
nekomi2 <dore123222@gmail.com>
2023-11-02 17:02:02 -0400

    refactor

--- frontend/pages/video/index.tsx
+++ frontend/pages/video/index.tsx
@@ -9,8 +9,8 @@ const videoOptions = {
   fill: true,
   sources: [
     {
-      src: "https://stream.mux.com/GJjLF93MGEmq4VfidIdZ4oMMAJRhEjSQ.m3u8",
-      type: "application/x-mpegURL"
+      src: "https://kwdmufkexqqxupbctksg.supabase.co/storage/v1/object/sign/demo/public/Me%20at%20the%20zoo.mp4?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJkZW1vL3B1YmxpYy9NZSBhdCB0aGUgem9vLm1wNCIsImlhdCI6MTY5ODM1Mzg4NiwiZXhwIjoxNzI5ODg5ODg2fQ.dfwfj6lM8rujyFmeLOJWsG-rRRwSHMLabOtwGCJhgR4&t=2023-10-26T20%3A58%3A07.114Z",
+      type: "video/mp4"
     }
   ]
 };
