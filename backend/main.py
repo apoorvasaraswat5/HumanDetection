d24e7af9b291da0b265ce6118479a32c83667de0
matt-weiner <mweiner@umass.edu>
2023-11-08 17:48:05 -0500

    add audio to combined endpoint

--- backend/main.py
+++ backend/main.py
@@ -156,14 +156,13 @@ def query(filename: str, local=True):
 @app.post("/process")
 def process_video(file_path):
     video = utils.download_file_by_path(file_path)
-    temp_wav_path,temp_video_path = utils.extract_audio(video)
+    temp_wav_path = utils.extract_audio(video)
 
+    video_results = None
     audio_results = whisper_diarization(temp_wav_path)
-    utils.upload_audio(audio_results,file_path)
-    video_results = detect_person(temp_video_path,file_path)
-
+    
     os.remove(temp_wav_path)
-    os.remove(temp_video_path)
+    
     return {"audio": audio_results, "video": video_results}
     
     
@@ -193,6 +192,7 @@ def diarize(filename: str):
 @app.post("/transcribe")
 def transcribe(filename: str):
     return whisper_diarization(filename)
+
 
 
 @app.post("/transcribe")
--- backend/utils.py
+++ backend/utils.py
@@ -3,6 +3,112 @@ import os
 # make sure you install ffmpeg for it to work
 
 
+load_dotenv()
+
+BUCKET_NAME = "human-detection-video-files"
+
+TABLE_NAME = "video-files"
+
+
+supabase = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_KEY"))
+
+
+def get_supabase():
+    return supabase
+
+
+def upload_file(file):
+    uuid_val = uuid.uuid4()
+    s3_key_video = f"videos/{file.filename}_{uuid_val}"
+    user_data = supabase.auth.get_user()
+    # in the future throw auth error if user doesn't exist
+    user_id = 0
+    if user_data:
+        user_id = user_data.user.id
+
+    thumbnail = create_thumbnail(file)
+    s3_key_thumbnail = f"thumbnails/{file.filename}_{uuid_val}_thumbnail"
+    file.file.seek(0)
+    video_content = file.file.read()
+    supabase.storage.from_(BUCKET_NAME).upload(
+        path=s3_key_video,
+        file=video_content,
+        file_options={"content-type": file.content_type},
+    )
+    supabase.storage.from_(BUCKET_NAME).upload(
+        path=s3_key_thumbnail,
+        file=thumbnail,
+        file_options={"content-type": "image/x-png"},
+    )
+    data, count = (
+        supabase.table(TABLE_NAME)
+        .insert(
+            {
+                "filename": file.filename,
+                "video_path": s3_key_video,
+                "thumbnail_path": s3_key_thumbnail,
+                "user_id": user_id,
+            }
+        )
+        .execute()
+    )
+    return data
+
+def extract_audio(video):
+    temp_video = tempfile.NamedTemporaryFile(suffix=".mp4", delete=False)
+    temp_video.write(video)
+    temp_video_path = temp_video.name
+    temp_video.close()
+    
+    video_clip = VideoFileClip(temp_video_path)
+    audio_clip = video_clip.audio
+    
+    temp_wav = tempfile.NamedTemporaryFile(suffix=".wav", delete=False)
+    temp_wav_path = temp_wav.name
+   
+    audio_clip.write_audiofile(temp_wav_path)
+    temp_wav.close()
+
+    
+    video_clip.close()
+    audio_clip.close()
+    
+    os.remove(temp_video_path)
+    return temp_wav_path
+
+def create_thumbnail(file):
+    temp_file_path = tempfile.NamedTemporaryFile(delete=False).name
+    with open(temp_file_path, "wb") as temp_file:
+        shutil.copyfileobj(file.file, temp_file)
+
+    with BytesIO() as thumbnail_buffer:
+        with VideoFileClip(temp_file_path) as video:
+            thumbnail = video.get_frame(0)
+            thumbnail_image = Image.fromarray(thumbnail)
+            thumbnail_image.save(thumbnail_buffer, format="PNG")
+            thumbnail_buffer.seek(0)
+        os.remove(temp_file_path)
+        return thumbnail_buffer.getvalue()
+            
+def download_file_by_path(file_path):
+    return supabase.storage.from_(BUCKET_NAME).download(file_path)
+
+
+
+def get_keys_for_user(user_id):
+    data, count = (
+        supabase.table(TABLE_NAME).select("s3_key").eq("user_id", user_id).execute()
+    )
+    return data
+
+
+
+def get_data(user_id):
+    data,count = supabase.table(TABLE_NAME).select('*').eq("user_id",user_id).execute()
+    return data
+
+
+# audio helper functions
 def convert_to_wav(filename: str):
     name, ext = os.path.splitext(filename)
     path = f"{name}.wav"
