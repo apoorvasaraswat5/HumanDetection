ba6f4f40e2205fc85ee93ef435823125733cd0ae
matt-weiner <mweiner@umass.edu>
2023-11-20 17:51:13 -0500

    upload audio res

--- backend/main.py
+++ backend/main.py
@@ -162,7 +162,7 @@ def process_video(file_path):
 
     video_results = detect_person(temp_video_path,file_path)
     audio_results = whisper_diarization(temp_wav_path)
-    
+    utils.upload_audio(audio_results,file_path)
     os.remove(temp_wav_path)
     os.remove(temp_video_path)
     return {"audio": audio_results, "video": video_results}
