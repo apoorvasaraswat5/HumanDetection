dcb14b54e8124fd148b9eb723248a7b8fd002793
rwbakerUMASS <rwbaker@umass.edu>
2023-11-30 18:45:52 -0500

    Add videoArtifacts to video player

--- frontend/app/upload/components/RecentUpload.tsx
+++ frontend/app/upload/components/RecentUpload.tsx
@@ -7,21 +7,35 @@ export default function RecentUpload({
     fileName = "video.mp4",
     date = "01/01/2020",
     processed = "Not Processed",
-    video_path = ""
+    video_path = "",
+    image_path = []
   }: {
-    onClick: (videoPath: string) => (event:any) => void;
+    onClick: (videoPath: any) => (event:any) => void;
     thumbnail?: string;
     fileName?: string;
     date?: string;
     processed?: string;
-    video_path: string
+    video_path: string;
+    image_path: string[]
   }) {
     thumbnail = 'http://127.0.0.1:8000/download?file_path=' + encodeURIComponent(thumbnail);
     video_path = 'http://127.0.0.1:8000/download?file_path=' + encodeURIComponent(video_path);
     let date_obj = new Date(date);
+    let videoArtifacts = {
+      videoURL:video_path,
+      peopleDetectedFrames: image_path? image_path.map((x: string, index: number) => {
+      return {
+        thumbnail: 'http://127.0.0.1:8000/download?file_path=' + encodeURIComponent(x),
+        timestamp: `00:00:${(index * 10 % 5).toString().padStart(2,'0')}` //Simulates timestamp
+      }
+    }):null,
+    distinctPeopleDetected: [],
+    voiceDetectedFrames: [],
+  }
+  console.log(videoArtifacts)
     return (
       <button
-        onClick={onClick(video_path)}
+        onClick={onClick(videoArtifacts)}
         className="flex w-full group items-center p-6 bg-white border border-gray-200 shadow hover:bg-blue-200 flex space-x-5">
         <Container>
             <Row>
