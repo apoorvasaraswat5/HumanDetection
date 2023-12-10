26a833379b215ea156b78551f1a0df6eec03d7e1
nekomi2 <dore123222@gmail.com>
2023-12-10 18:23:18 -0500

    add colorcoding

--- frontend/components/AudioTranscript.tsx
+++ frontend/components/AudioTranscript.tsx
@@ -1,19 +1,31 @@
 import { VideoArtifacts, VoiceDetectedFrame } from "@/utils/api";
 import { Fragment } from "react";
+
 export default function TranscriptCard({
   currentVideoArtifacts,
 }: {
   currentVideoArtifacts: VideoArtifacts;
 }) {
   const transcriptLines = currentVideoArtifacts.voiceDetectedFrames?.map(
-    (frame, index) => (
-      <Fragment key={index}>
-        {`[${Number(frame.startTime).toFixed(2)} - ${Number(
-          frame.endTime
-        ).toFixed(2)}] ${frame.speaker}: ${frame.transcript}`}
-        <br />
-      </Fragment>
-    )
+    (frame, index) => {
+      let textColor = "";
+      if (frame.sentiment === "negative") {
+        textColor = "text-red-500"; // Red color for negative sentiment
+      } else if (frame.sentiment === "positive") {
+        textColor = "text-green-500"; // Green color for positive sentiment
+      }
+
+      return (
+        <Fragment key={index}>
+          <span className={textColor}>
+            {`[${Number(frame.startTime).toFixed(2)} - ${Number(
+              frame.endTime
+            ).toFixed(2)}] ${frame.speaker}: ${frame.transcript}`}
+          </span>
+          <br />
+        </Fragment>
+      );
+    }
   );
 
   return (
